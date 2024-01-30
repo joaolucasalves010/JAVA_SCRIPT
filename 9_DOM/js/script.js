deb50950e0dcc736467eff0c0165074dbf9e655c
joaolucasalves010 <ylucasslalves@gmail.com>
2024-01-30 19:38:33 -0300

    HTML e js

--- 9_DOM/js/script.js
+++ 9_DOM/js/script.js
@@ -60,3 +60,18 @@ são todos os elementos visíveis para o usuário
 - As tags possuem atributos que configuram elementos
 
 */
+
+// 5 - HTML e JavaScript
+
+/*
+
+- Podemos adicionar JavaScript ao HTML por meio da tag <script></script>, em arquivo externo ou script na página
+
+- Algumas tags tem atributos que podem executar JS, mas isso não é muito utilizado
+
+- Sempre que houver um link entre um arquivo e outro uma chamada HTTP é executada
+
+- JavaScript pode ser utilizado para manipular elementos do HTML e alterar estilos
+
+
+*/
