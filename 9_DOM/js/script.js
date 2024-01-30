d6bc4d4d31e4f910bd3542e2d8178f303c3ad366
joaolucasalves010 <ylucasslalves@gmail.com>
2024-01-30 18:33:13 -0300

    Protocolos HTTP

--- 9_DOM/js/script.js
+++ 9_DOM/js/script.js
@@ -1,2 +1,15 @@
+// 1 - Protocolos da web
 
+/*
 
+- Um protocolo é uma forma de comunicação entre computadores através da rede 
+
+- O HTTP serve para solicitar arquivos e imagens do servidor (Hyper Text Transfer Protocol)
+
+- É possível navagar em sites através do HTTP
+
+- SMTP: Protocolo para envio de email
+
+- TCP: Protocolo para transferência de dados
+
+*/
