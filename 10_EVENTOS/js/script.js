4ef6bd0757d02165cfd8917ec83430a62b9717dd
joaolucasalves010 <ylucasslalves@gmail.com>
2024-02-15 09:51:33 -0300

    Removendo Eventos

--- 10_EVENTOS/js/script.js
+++ 10_EVENTOS/js/script.js
@@ -14,4 +14,54 @@
 
 */
 
-aaa
+// 2 - Adicionando eventos
+
+/*
+
+- Primeiramente precisamos selecionar o elemento que vai disparar
+o evento
+
+- Depois vamos ativar um método chamado addEventListener
+
+- Nele declaramos qual o tipo do evento, e por meio de callback
+definimos o que acontece
+
+*/
+
+const btn = document.querySelector('#my-button')
+
+btn.addEventListener('click', function() {
+
+    console.log('Clicou no botão')
+
+    // Modificando o botão 
+    btn.style.color = 'green'
+    btn.style.background = 'hsl(0, 0%, 25%)'
+})
+
+// 2 - Removendo Eventos
+
+/*
+
+- Há situações que vamos querer remover os eventos dos elementos
+
+- O método para isso é removeEventListener
+
+- Passamos o evento que queremos remover como argumento
+
+*/
+
+const secondBtn = document.querySelector('#btn')
+
+function imprimirMensagem() {
+    console.log('teste')
+}
+
+secondBtn.addEventListener('click', imprimirMensagem)
+
+const thirdBtn = document.querySelector('#other-btn')
+
+thirdBtn.addEventListener('click', () => {
+    console.log('Evento Removido')
+    secondBtn.removeEventListener('click', imprimirMensagem)
+})
