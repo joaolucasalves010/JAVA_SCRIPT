c0e99bef656195d9fd4288387766b48374b96b30
joaolucasalves010 <ylucasslalves@gmail.com>
2024-03-13 22:52:08 -0300

    Herança

--- 11_ES6/js/scripts.js
+++ 11_ES6/js/scripts.js
@@ -137,3 +137,95 @@ const productDetails = {
 const {name: productName, price, category, color} = productDetails
 
 console.log(`O nome do produto é ${productName}, ele custa ${price} e é da cor ${color}`)
+
+// 7 - spread operator
+    
+const a1 = [1, 2, 3]
+const a2 = [4, 5, 6]
+
+const a3 = [...a1, ...a2];
+
+console.log(a3)
+
+const a4 = [0, ...a3, 7];
+
+console.log(a4)
+
+const carName = {name: 'Gol'}
+const carBrand = {brand: "Vw"}
+const otherInfos = {km: 100000, price: 49999}
+
+const car = {...carName, ...carBrand, ...otherInfos}
+
+console.log(car)
+
+// Outras maneiras de unir array utilizando concat e sort para deixar do menor elemento para o maior
+
+// 8 - classes
+
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+    productWithDiscount(discount) {
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const camiseta = new Product('Camisa gola polo', 20)
+
+console.log(camiseta)
+console.log(camiseta.productWithDiscount(10))
+console.log(camiseta.productWithDiscount(15))
+
+// 9 - Herança
+
+class ProductWithAtributes extends Product {
+
+    constructor(name, price, colors) {
+        super(name, price)
+        this.colors = colors
+    }
+ 
+    showColors() {
+        this.colors.forEach((color) => {
+            console.log(color)
+        })
+    }
+    
+}
+
+const hat = new ProductWithAtributes('Chapeu', 29.99, ['Preto', 'Amarelo', 'Verde'])
+
+console.log(hat.name)
+
+console.log(hat.showColors())
+
+console.log(hat.price)
+
+class Dog {
+    constructor() {
+        this.patas = 4
+        this.type = 'Mamifero'
+    }
+}
+
+class NewDog extends Dog {
+    constructor(patas, type, color, name) {
+        super(patas, type)
+        this.color = color
+        this.name = name
+    }
+}
+
+const glock = new NewDog(4, 'Mamifero', 'Preto', 'Glock')
+
+const array = []
+
+array.push(glock)
+
+array.forEach((dog) => {
+    console.log(dog)
+})
+
